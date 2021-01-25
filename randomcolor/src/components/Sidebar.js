178b3ba7dca7c1cc7322ac414ae55123299d9a7f
thomalexg <thomas.gae@posteo.de>
2021-01-25 16:46:25 +0100

    Able to change the background inside sidebar

--- randomcolor/src/components/Sidebar.js
+++ randomcolor/src/components/Sidebar.js
@@ -1,29 +1,32 @@
 import React, { useState } from 'react';
 
-const hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
-
-let hex = '';
-const randomHex = () => {
-  hex += '#';
-  for (let i = 1; i <= 6; i++) {
-    let random = Math.floor(Math.random() * 16);
-    hex += hexArr[random];
-  }
-  return hex;
-};
-
 //randomHex();
 export default function SideBar(props) {
-  const [random, setRandomHex] = useState('');
+  const [random, setRandomHex] = useState('#0f664f');
+  const hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+
+  let hex = '';
+  const randomHex = () => {
+    hex += '#';
+    for (let i = 1; i <= 6; i++) {
+      let random = Math.floor(Math.random() * 16);
+      hex += hexArr[random];
+    }
+    return hex;
+  };
+
   return (
-    <div className="SideBar">
+    <div className="SideBar" style={{ backgroundColor: `${random}` }}>
       <button
         className="SideBarButton"
-        onClick={() => setRandomHex(randomHex())}
+        onClick={() => {
+          hex = '';
+          setRandomHex(randomHex());
+        }}
       >
         Generate random color hex code
       </button>
-      <p>The hexcode is: {hex} </p>
+      <p>The hexcode is: {random} </p>
     </div>
   );
 }
